b09c6745b3f16b2dcf04053af5d59ceba8ed2400
royanirudd <aniruddroy704@gmail.com>
2025-07-08 19:49:28 +0000

    Adde Lazygit + modicator
    TODO: Fix modicator colorscheme
    Fix lazygit icons

---
+++ lua/plugins/modicator.lua
@@ -0,0 +1,16 @@
+return {
+	"mawkler/modicator.nvim",
+	dependencies = "ribru17/bamboo.nvim", -- Add your colorscheme plugin here
+	init = function()
+		vim.o.cursorline = true
+		vim.o.number = true
+		vim.o.termguicolors = true
+	end,
+	opts = {
+		-- Warn if any required option above is missing. May emit false positives
+		-- if some other plugin modifies them, which in that case you can just
+		-- ignore. Feel free to remove this line after you've gotten Modicator to
+		-- work properly.
+		show_warnings = true,
+	},
+}
