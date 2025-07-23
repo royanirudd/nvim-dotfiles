20433744f8fc6cfabe28fb4114366e3c6bf3665c
royanirudd <aniruddroy704@gmail.com>
2025-07-23 14:06:37 +0000

    Updated telekasten to be lazily loaded
    Ignoring .bak files

--- lua/plugins/zettelkasten.lua
+++ lua/plugins/zettelkasten.lua
@@ -1,17 +1,17 @@
 return {
-	-- Point to the new, community-maintained version
 	"nvim-telekasten/telekasten.nvim",
 
 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 
-	-- This 'opts' table is the standard lazy.nvim way to pass options.
-	-- It is more reliable than putting the setup call inside the config function.
+	lazy = true,
 	---@type telekasten.Config
 	opts = {
-		-- The crucial option that will now be correctly read
 		filetype_ft = "telekasten.markdown",
 
-		-- All your other settings
+		extras = {
+			"graph",
+		},
+
 		home = "/mnt/d/zettelkasten",
 		extension = ".md",
 		notes = "notes",
@@ -25,10 +25,4 @@ return {
 		source_note_header = "References",
 		related_topics_header = "Related Topics",
 	},
-
-	-- The config function now becomes extremely simple.
-	-- It just calls setup, and lazy.nvim automatically passes the 'opts' table to it.
-	config = function(_, opts)
-		require("telekasten").setup(opts)
-	end,
 }
